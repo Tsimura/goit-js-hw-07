086b67b2fe46d5584f37d369592167017f9f1221
Tsimura <chappa2828@gmail.com>
2021-10-05 23:50:15 +0300

    start task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryImagesEl = document.querySelector('div.gallery');
+const galleryMarkup = createImgGallery(galleryItems);
+
+galleryImagesEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryImagesEl.addEventListener('click', onGalleryCatchClick);
+
+function createImgGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+  `;
+    })
+    .join('');
+}
+
+function onGalleryCatchClick(e) {
+  console.log(e.target);
+}
+//-----------//
 console.log(galleryItems);
+
+// https://youtu.be/pNxBoxbnTVA?t=4188
+
+// https://youtu.be/kxwN7eXBNDQ?t=3693
+
+// *https://youtu.be/kxwN7eXBNDQ?t=4928
